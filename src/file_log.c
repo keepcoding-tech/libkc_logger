7381dec978655a23fdc2cb7b39aa80daed02049c
matteobagnolini <142099002+matteobagnolini@users.noreply.github.com>
2023-08-14 15:20:33 +0200

    Update file_log.c, removed log_ prefixes

--- src/file_log.c
+++ src/file_log.c
@@ -1,86 +1,80 @@
 #include "../include/console_log.h"
-#include "../include/file_log.h"
 
-#include <time.h>
+#include <assert.h>
+#include <string.h>
 
-// MARK: PUBLIC MEMBER METHODS PROTOTYPES
-void log_to_file(const char* file_path, const char* log, const char* message,
-    const char* file, const int line, const char* func);
-
-// MARK: CONSTRUCTOR & DESTRUCTOR DEFINITIONS
-
-// The constructor should be used to create a new instance
-struct FileLog* new_file_log() {
-  // create a KClog instance to be returned
-  struct FileLog* new_log = malloc(sizeof(struct FileLog));
-
-  // confirm that there is memory to allocate
-  if (new_log == NULL) {
-    struct ConsoleLog* log = new_console_log();
-    log->error("OUT_OF_MEMORY", "Failing to allocate memory "
-        "dynamically (using 'malloc') due to insufficient memory in the heap.",
-        __FILE__, __LINE__, __func__);
-    return NULL;
-  }
-
-  // assign the public member methods
-  new_log->log_to_file = log_to_file;
-
-  return new_log;
+// Test the creation and destruction of an instance.
+void test_creation_and_destruction() {
+  struct ConsoleLog* log = new_console_log();
+  destroy_console_log(log);
 }
 
-// The destructor should be used to destroy an existing instance
-void destroy_file_log(struct FileLog* log) {
-  // free the memory only if the log is not null reference
-  if (log == NULL) {
-    struct ConsoleLog* log = new_console_log();
-    log->warning("NULL_REFERENCE", "Attempting to use a reference "
-        "or pointer that points to NULL, or is uninitialized.",
-        __FILE__, __LINE__, __func__);
-    return;
-  }
+// Test case for the log_debug() method.
+void test_log_debug() {
+  struct ConsoleLog* log = new_console_log();
 
-  free(log);
+  // log the error to the console
+  log->debug("THIS IS JUST A TEST!!!", "This is a test description! XD",
+      __FILE__, __LINE__, __func__);
+
+  destroy_console_log(log);
 }
 
-// MARK: PUBLIC MEMBER METHODS DEFINITIONS
+// Test case for the log_error() method.
+void test_log_error() {
+  struct ConsoleLog* log = new_console_log();
 
-// This function will write a message to a specified file.
-void log_to_file(const char* filename, const char* log, const char* message,
-    const char* file, const int line, const char* func) {
-  // open the file in append mode (creates a new file if it doesn't exist)
-  FILE *write_file = fopen(filename, "a");
+  // log the error to the console
+  log->error("THIS IS JUST A TEST!!!", "This is a test description! XD",
+      __FILE__, __LINE__, __func__);
 
-  // print an error and exit the program
-  if (write_file == NULL) {
-    struct ConsoleLog* log = new_console_log();
-    char error_description[256];
-    sprintf(error_description, "Failed to open the specified file: %s", filename);
-    log->error("CANNOT_OPEN_FILE", error_description,
-        __FILE__, __LINE__, __func__);
-
-    // exit the program
-    exit(1);
-  }
-
-  // get the current timestamp
-  time_t now;
-  struct tm *tm_info;
-  char time_buffer[30];
-  time(&now);
-
-  // convert the timestamp to a human-readable string
-  tm_info = localtime(&now);
-  strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d %H:%M:%S", tm_info);
-
-  // writing data the log to the file
-  fprintf(write_file, "\n");
-  fprintf(write_file, "[%s]\n", time_buffer);
-  fprintf(write_file, "%s: in function ‘%s’\n", file, func);
-  fprintf(write_file, "%s:%d message: %s\n", file, line, log);
-  fprintf(write_file, "  %s\n", message);
-
-  // close the file
-  fclose(write_file);
+  destroy_console_log(log);
 }
+
+// Test case for the log_message() method.
+void test_log_message() {
+  struct ConsoleLog* log = new_console_log();
+
+  // log the message to the console
+  log->message("THIS IS JUST A TEST!!!", "If you see this message, don't "
+      "worry, this is just a test. :D \nActually, seeing this means the test "
+      "is working as it should.",
+      __FILE__, __LINE__, __func__);
+
+  destroy_console_log(log);
+}
+
+// Test case for the log_warning() method.
+void test_log_warning() {
+  struct ConsoleLog* log = new_console_log();
+
+  // log the warning to the console
+  log->warning("THIS IS JUST A TEST!!!", "This is a test description! XD",
+      __FILE__, __LINE__, __func__);
+
+  destroy_console_log(log);
+}
+
+// Test case for the log_fatal() method.
+void test_log_fatal() {
+  struct ConsoleLog* log = new_console_log();
+
+  // log the error to the console
+  log->fatal("THIS IS JUST A TEST!!!", "After this test, the program "
+      "will exit with a status code of 1. This is GOOD!!",
+      __FILE__, __LINE__, __func__);
+
+  destroy_console_log(log);
+}
+
+int main() {
+  test_creation_and_destruction();
+  test_log_debug();
+  test_log_error();
+  test_log_message();
+  test_log_warning();
+  test_log_fatal();
+  return 0;
+}
+
 
